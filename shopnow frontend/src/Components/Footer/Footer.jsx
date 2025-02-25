c5cd2c16509b5513f4d5af73301d7a7688a29ea6
Gabriel Reis <gabrielreistech@gmail.com>
2025-02-25 01:24:11 -0300

    Adição e estilização dos componentes Header e Footer

---
+++ shopnow frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return(
+      <div className={styles.container}>
+        <h2>Uma frase aleatória, apenas pra preencher o espaço.</h2>
+      </div>
+    );
+}
+export default Footer;
--- shopnow frontend/src/main.jsx
+++ shopnow frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Router from "./Router"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
      <>
-      <h1>Hello, World!</h1>
+      <Router />
      </>
 
   </StrictMode>,
