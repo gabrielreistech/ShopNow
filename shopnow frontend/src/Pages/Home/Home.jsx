b2adbe2af0b942471cbf0641b22d264a7c70ac12
Gabriel Reis <gabrielreistech@gmail.com>
2025-02-26 22:07:54 -0300

    feat: criar componentes MainLayout e Card

--- shopnow frontend/src/Pages/Home/Home.jsx
+++ shopnow frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import Header from "../../Components/Header/Header"
 import Footer from "../../Components/Footer/Footer"
+import Card from "../../Components/Card/Card";
+import MainLayout from "../../Components/MainLayout/MainLayout";
 
 const Home = () => {
      
     return(
         <>
         <Header />
+        <MainLayout>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        </MainLayout>
         <Footer />
         </>
     );
