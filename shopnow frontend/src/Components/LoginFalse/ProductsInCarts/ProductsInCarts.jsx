68dd47aac6583c259bee2aa32adb1a33a4bb2c39
Gabriel Reis <gabrielreistech@gmail.com>
2025-04-13 10:29:41 -0300

    feat: concluir redimensionamento do Header com ajustes

--- shopnow frontend/src/Components/LoginFalse/ProductsInCarts/ProductsInCarts.jsx
+++ shopnow frontend/src/Components/LoginFalse/ProductsInCarts/ProductsInCarts.jsx
@@ -5,10 +5,14 @@ const ProductsInCarts = ({ produto }) => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.nome}>Nome: {produto.nome}</div>
-            <div className={styles.preco}>Preço: R${produto.preco}</div>
-            <div className={styles.quantidadeNoCarrinho}>Quantidade <br /> no carrinho: {produto.quantidadeNoCarrinho}</div>
-            <AdicionarERemoverDoCarrinho produto={produto} />
+            <div className={styles.containerDasVariaveis}>
+                <div className={styles.nome}>Nome: {produto.nome}</div>
+                <div className={styles.preco}>Preço: R${produto.preco}</div>
+                <div className={styles.quantidadeNoCarrinho}>Total no carrinho: {produto.quantidadeNoCarrinho}</div>
+            </div>
+            <div className={styles.containerDoBotao}>
+                <AdicionarERemoverDoCarrinho produto={produto} />
+            </div>
         </div>
     );
 }
