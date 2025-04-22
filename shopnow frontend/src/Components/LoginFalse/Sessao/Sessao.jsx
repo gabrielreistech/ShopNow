a94cfa151a821a356ddbe8e0bb839ff006de50b8
Gabriel Reis <gabrielreistech@gmail.com>
2025-04-21 23:30:16 -0300

    feat: implementar script para detecção de tela em mobile

--- shopnow frontend/src/Components/LoginFalse/Sessao/Sessao.jsx
+++ shopnow frontend/src/Components/LoginFalse/Sessao/Sessao.jsx
@@ -1,43 +1,47 @@
 import { Link } from "react-router-dom";
 import styles from "./Sessao.module.css";
+import ViewportInfo from "../../LoginFalse/ViewportInfo/ViewportInfo";
 
 const Sessao = () => {
-    return(
-       <section className={styles.container}>
+  return (
+    <section className={styles.container}>
 
-          <div className={styles.sections}>
-           <Link to="/login">
-            <p>Entre na sua conta</p>
-            <img src="/conta.png" alt="#" />
-            <p>Aproveite ofertas para <br /> comprar tudo que quiser.</p>
-            </Link>
-          </div>
+      <div className={styles.sections}>
+        <Link to="/login">
+          <p>Entre na sua conta</p>
+          <img src="/conta.png" alt="#" />
+          <p>Aproveite ofertas para <br /> comprar tudo que quiser.</p>
+        </Link>
+      </div>
 
-          <div className={styles.sections}>
-            <Link to="/meiosdepagamentos">
-            <p>Meios de Pagamentos</p>
-            <img src="/moeda-de-dolar (3).png" alt="#" />
-            <p>Pague suas compras com<br /> rapidez e segurança.</p>
-            </Link>
-          </div>
+      <div className={styles.sections}>
+        <Link to="/meiosdepagamentos">
+          <p>Meios de Pagamentos</p>
+          <img src="/moeda-de-dolar (3).png" alt="#" />
+          <p>Pague suas compras com<br /> rapidez e segurança.</p>
+        </Link>
+      </div>
 
-          <div className={styles.sections}>
-            <Link to="/produtosabaixode100">
-            <p>Abaixo de R$100</p>
-            <img src="/preco-baixo.png" alt="#" />
-            <p>Confira produtos<br /> com preços baixos.</p>
-            </Link>
-          </div>
+      <ViewportInfo />
 
-          <div className={styles.sections}>
-            <Link to="/sobre">
-            <p>Sobre nós</p>
-            <img src="/quadrado.png" alt="#" />
-            <p>Conheça quem nós somos <br /> e os nossos propósitos.</p>
-            </Link>
-          </div>
-       </section>
-    );
+      <div className={styles.sections}>
+        <Link to="/produtosabaixode100">
+          <p>Abaixo de R$100</p>
+          <img src="/preco-baixo.png" alt="#" />
+          <p>Confira produtos<br /> com preços baixos.</p>
+        </Link>
+      </div>
+
+      <div className={styles.sections}>
+        <Link to="/sobre">
+          <p>Sobre nós</p>
+          <img src="/quadrado.png" alt="#" />
+          <p>Conheça quem nós somos <br /> e os nossos propósitos.</p>
+        </Link>
+      </div>
+    </section>
+  );
 }
 
 export default Sessao;
+
